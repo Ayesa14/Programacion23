702d5e067f6172ce657826d762591b36a8013251
Ayesa Marin <amarinconde@danielcastelao.org>
2023-10-02 10:21:27 +0200

    Añadimos src

---
+++ Boletin2/Boletin2_5/src/boletin2_5/Boletin2_5.java
@@ -0,0 +1,22 @@
+package boletin2_5;
+//Escribe un programa que lea o valor dunha distancia en millas mariñas e a pase a metros 
+//( 1 milla mariña = 1852 m ).
+
+import java.util.Scanner;
+
+
+public class Boletin2_5 {
+
+    
+    public static void main(String[] args) {
+        float millas;
+        System.out.println("Introduce el número de millas marinas:");
+        Scanner millasMarinas = new Scanner(System.in);
+        millas = millasMarinas.nextFloat();
+        
+        float metros = millas * 1852;
+        System.out.println(millas+" millas marinas son "+metros+"metros");
+        
+    }
+    
+}
