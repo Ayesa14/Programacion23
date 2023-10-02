ebd7921b44e70f822456276c0b4dddc490a7377f
Ayesa Marin <amarinconde@danielcastelao.org>
2023-10-02 10:19:48 +0200

    Añadimos src

---
+++ Boletin2/Boletin2_2/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,15 @@
+package boletin2_2;
+//Realiza un programa que permita calcular o área dun cadrado de 3m de lado
+
+public class Boletin2_2 {
+
+   
+    public static void main(String[] args) {
+        float lado = 3;
+        float area = (float) Math.pow(lado, 2);
+        
+        System.out.println("El área del cuadrado es: "+area);
+        
+    }
+    
+}
