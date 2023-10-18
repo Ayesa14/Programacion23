d16dff2b9bfc90fd8e8fb6cf9a6096b3e57698f3
Ayesa Marin <amarinconde@danielcastelao.org>
2023-10-18 10:47:22 +0200

    Añadimos boletin6

---
+++ Boletin06/Boletin6_1/src/boletin6_1/Boletin6_1.java
@@ -0,0 +1,24 @@
+package boletin6_1;
+
+import java.util.Scanner;
+public class Boletin6_1 {
+
+    public static void main(String[] args) {
+        Scanner coche = new Scanner(System.in);
+        int velocidade, valor, menos;
+        
+        //instanciamos objeto coche sin parámetros
+        Coche objCoche = new Coche();
+        
+        objCoche.setVelocidade(150);
+        
+        System.out.println("Ingrese el valor de la aceleración: ");
+        valor = coche.nextInt();
+        objCoche.acelerar(valor);
+        
+        System.out.println("Ingrese el valor de frenado: ");
+        menos = coche.nextInt();
+        objCoche.frenar(menos);
+    }
+    
+}
---
+++ Boletin06/Boletin6_3/src/boletin6_3/Boletin6_3.java
@@ -0,0 +1,13 @@
+package boletin6_3;
+
+public class Boletin6_3 {
+
+    public static void main(String[] args) {
+        //instanciamos objeto parametrizado
+        Circulo c = new Circulo(6.5);
+        
+        c.calcularArea();
+        c.calcularLonxitude();
+    }
+    
+}
